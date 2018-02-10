ae53b55e58e78549b989526b5ae105d9a76aba72
Karthik Vegi <karthikvegi@outlook.com>
2018-02-10 04:08:37 -0500

    reusable functions for validating fields

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -6,20 +6,25 @@ source = open(sys.argv[1], "r")
 percentile = open(sys.argv[2], "r")
 destination = open(sys.argv[3], "w")
 
+campaign_data = []
+
 def read_from_source(source):
-    campaign_contributions = source.read()
+    global campaign_data
+    campaign_data = source.read()
 
 def write_to_destination(record, destination):
     destination.write("|".join(record) + "\n")
 
-def invalid_fields(record):
-    fields = record.split("|")
-    # Check empty fields
+def empty_fields(fields):
     if any(map(lambda x: not x.strip(), fields)):
         return True
-    if len(fields[2].strip()) < 5:
+
+def malformed_field(field, ideal_length):
+    if len(field) < ideal_length:
         return True
+
+def invalid_date(field, format):
     try:
-        datetime.strptime(fields[3], '%m%d%Y')
+        datetime.strptime(field, format)
     except Exception as e:
         return True
