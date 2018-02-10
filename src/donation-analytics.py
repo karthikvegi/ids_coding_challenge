cb8fc1a15a5af92d1e8ea06e1e6659987e514120
Karthik Vegi <karthikvegi@outlook.com>
2018-02-09 20:32:00 -0500

    added function: read_from_source

---
+++ src/donation-analytics.py
@@ -0,0 +1,9 @@
+import sys
+
+# Input/Output
+source = open(sys.argv[1], "r")
+percentile = open(sys.argv[2], "r")
+destination = open(sys.argv[3], "w")
+
+def read_from_source(source):
+    campaign_contributions = source.read()
