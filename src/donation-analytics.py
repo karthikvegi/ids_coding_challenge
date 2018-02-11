0ff5cf9eb1e1517e52efa0e730b49b39f10de1d2
Karthik Vegi <karthikvegi@outlook.com>
2018-02-11 18:19:59 -0500

    updated process_data: fixed bug wrt handling records with no chronological order

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -11,7 +11,7 @@ from collections import defaultdict
 # Input/Output
 try:
     source = open(sys.argv[1], "r")
-    pct_file = open(sys.argv[2], "r")
+    percentile_source = open(sys.argv[2], "r")
     destination = open(sys.argv[3], "w")
 except Exception as e:
     print(e)
@@ -37,8 +37,15 @@ def invalid_date(field, format):
 def valid_record(record):
     zip_code = record[2]
     transaction_dt = record[3]
-    if not empty_fields(record) and not malformed_field(zip_code, 5) and not invalid_date(transaction_dt, '%m%d%Y'):
+    if not empty_fields(record) \
+            and not malformed_field(zip_code, 5) \
+            and not invalid_date(transaction_dt, '%m%d%Y'):
         return True
+
+def compute_percentile(donations, percentile):
+    # Nearest-rank method
+    idx = math.ceil(percentile * 0.01 * len(donations))
+    return donations[idx-1]
 
 def ingest_record(row):
     fields = row.split("|")
@@ -48,14 +55,9 @@ def ingest_record(row):
     if other_id.strip() == "" and valid_record(record):
         return record
 
-def compute_percentile(donations, percentile):
-    # Nearest-rank method
-    idx = math.ceil(percentile * 0.01 * len(donations))
-    return donations[idx-1]
-
 def process_data(source):
     campaign_data = source.read()
-    percentile = float(pct_file.read())
+    percentile = float(percentile_source.read())
     # Data Structures
     donor_list = {} # Key: donor_id | Value: transaction_yr
     donation_dict = defaultdict(list) # Key: recipient | Value: list of transactions
@@ -66,26 +68,30 @@ def process_data(source):
             continue
         recipient, donor, zip_code, transaction_dt, transaction_amt = record
         transaction_yr = int(transaction_dt[4:])
-        donor_id = donor + zip_code
+        donor_id = "-".join([donor, zip_code])
+        recipient_id = "-".join([recipient, zip_code, str(transaction_yr)])
 
         # New donor
         if donor_id not in donor_list:
             donor_list[donor_id] = transaction_yr
-            continue
         # Repeat donor if contributed in prior calendar year
+        # update transaction_yr if transaction found from prior calendar year
+        elif transaction_yr < donor_list[donor_id]:
+            donor_list[donor_id] = transaction_yr
         elif transaction_yr > donor_list[donor_id]:
             # Accumulate transactions as a list in the dictionary
-            donation_dict[recipient].append(int(transaction_amt))
-            transaction_cnt = len(donation_dict[recipient])
-            contribution = sum(donation_dict[recipient])
-            running_percentile = compute_percentile(donation_dict[recipient], percentile)
+            donation_dict[recipient_id].append(int(transaction_amt))
+            transaction_cnt = len(donation_dict[recipient_id])
+            contribution = sum(donation_dict[recipient_id])
+            running_percentile = compute_percentile(donation_dict[recipient_id], percentile)
+            
             output = [recipient, zip_code, str(transaction_yr), str(running_percentile), str(contribution), str(transaction_cnt)]
             write_to_destination(output, destination, '|')
 
 def clean_up():
     source.close()
     destination.close()
-    pct_file.close()
+    percentile_source.close()
 
 process_data(source)
 clean_up()
