7f815083d895c63b88f59a66311be9b1b8ad78e4
Karthik Vegi <karthikvegi@outlook.com>
2018-02-13 04:52:47 -0500

    fixed bug with donor_dict

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -13,6 +13,23 @@ def clean_up():
     destination.close()
     percentile_source.close()
 
+def ingest_record(row):
+    cols = row.split("|")
+    other_id = cols[15]
+    fields = [cols[0], cols[7], cols[10][:5], cols[13], cols[14]]
+    record = {}
+    # Process only individual contributions and valid records
+    if other_id.strip() == "" and valid_record(fields):
+        # Create a dictionary with fields for easy access
+        record['recipient'] = cols[0]
+        record['donor'] = cols[7]
+        record['zip_code'] = cols[10][:5]
+        record['transaction_yr'] = int(cols[13][4:])
+        record['transaction_amt'] = int(cols[14])
+        record['donor_id'] = record['donor'] + record['zip_code']
+        record['recipient_id'] = record['recipient'] + record['zip_code'] + str(record['transaction_yr'])
+        return record
+
 def valid_record(record):
     zip_code = record[2]
     transaction_dt = record[3]
@@ -21,51 +38,28 @@ def valid_record(record):
             and not invalid_date(transaction_dt, '%m%d%Y'):
         return True
 
-def ingest_record(row):
-    fields = row.split("|")
-    other_id = fields[15]
-    record = [fields[0], fields[7], fields[10][:5], fields[13], fields[14]]
-    # Process only individual contributions and valid records
-    if other_id.strip() == "" and valid_record(record):
-        return record
-
-def process_data(source, destination, percentile):
-    campaign_data = source.read()
-    valid_records = invalid_records = 0
+def process_data(source, destination):
     # Data Structures
-    donor_list = {} # Key: donor_id | Value: transaction_yr
+    donor_dict = {} # Key: donor_id | Value: transaction_yr
     donation_dict = defaultdict(list) # Key: recipient | Value: list of transactions
-
-    for row in campaign_data.splitlines():
+    for row in source.read().splitlines():
         record = ingest_record(row)
         if not record:
-            invalid_records += 1
             continue
-        valid_records += 1
-        recipient, donor, zip_code, transaction_dt, transaction_amt = record
-        transaction_yr = int(transaction_dt[4:])
-        transaction_amt = int(round(float(transaction_amt)))
-        donor_id = "-".join([donor, zip_code])
-        recipient_id = "-".join([recipient, zip_code, str(transaction_yr)])
-
         # New donor
-        if donor_id not in donor_list:
-            donor_list[donor_id] = transaction_yr
-        # Repeat donor
+        if record['donor_id'] not in donor_dict:
+            donor_dict[record['donor_id']] = record['transaction_yr']
         # update transaction_yr if you come across donation from a previous year
-        elif transaction_yr < donor_list[donor_id]:
-            donor_list[donor_id] = transaction_yr
-        # perform calculations
-        elif transaction_yr > donor_list[donor_id]:
+        elif record['transaction_yr'] < donor_dict[record['donor_id']]:
+            donor_dict[record['donor_id']] = record['transaction_yr']
+        elif record['transaction_yr'] > donor_dict[record['donor_id']]:
             # Accumulate contributions as a list in the dictionary
-            donation_dict[recipient_id].append(transaction_amt)
-            transaction_cnt = len(donation_dict[recipient_id])
-            contribution = sum(donation_dict[recipient_id])
-            running_percentile = compute_percentile(donation_dict[recipient_id], percentile)
-
-            output = [recipient, zip_code, str(transaction_yr), str(running_percentile), str(contribution), str(transaction_cnt)]
+            donation_dict[record['recipient_id']].append(record['transaction_amt'])
+            transaction_cnt = len(donation_dict[record['recipient_id']])
+            contribution = sum(donation_dict[record['recipient_id']])
+            pct = compute_percentile(donation_dict[record['recipient_id']], percentile)
+            output = [record['recipient'], record['zip_code'], str(record['transaction_yr']), str(pct), str(contribution), str(transaction_cnt)]
             write_to_destination(output, destination, '|')
-    print_summary_statistics([valid_records, invalid_records])
 
 if __name__ == "__main__":
     try:
@@ -80,6 +74,5 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
         sys.exit(1)
-
-    process_data(source, destination, percentile)
+    process_data(source, destination)
     clean_up()
