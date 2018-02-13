5c05a96d10310f26947874d2d815ec8e51ac0374
Karthik Vegi <karthikvegi@outlook.com>
2018-02-13 05:36:05 -0500

    created dict of variables for easy access

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -13,53 +13,50 @@ def clean_up():
     destination.close()
     percentile_source.close()
 
-def ingest_record(row):
-    cols = row.split("|")
-    other_id = cols[15]
-    fields = [cols[0], cols[7], cols[10][:5], cols[13], cols[14]]
-    record = {}
-    # Process only individual contributions and valid records
-    if other_id.strip() == "" and valid_record(fields):
-        # Create a dictionary with fields for easy access
-        record['recipient'] = cols[0]
-        record['donor'] = cols[7]
-        record['zip_code'] = cols[10][:5]
-        record['transaction_yr'] = int(cols[13][4:])
-        record['transaction_amt'] = int(cols[14])
-        record['donor_id'] = record['donor'] + record['zip_code']
-        record['recipient_id'] = record['recipient'] + record['zip_code'] + str(record['transaction_yr'])
-        return record
-
 def valid_record(record):
-    zip_code = record[2]
-    transaction_dt = record[3]
-    if not empty_fields(record) \
-            and not malformed_field(zip_code, 5) \
-            and not invalid_date(transaction_dt, '%m%d%Y'):
+    check_empty_fields = [record['transaction_dt'], record['zip_code'], record['donor'], record['recipient'], record['transaction_amt']]
+    if record['other_id'].strip() == "" \
+            and not empty_fields(check_empty_fields) \
+            and not malformed_field(record['zip_code'], 5) \
+            and not invalid_date(record['transaction_dt'], '%m%d%Y'):
         return True
 
-def process_data(source, destination):
-    # Data Structures
-    donor_dict = {} # Key: donor_id | Value: transaction_yr
-    donation_dict = defaultdict(list) # Key: recipient | Value: list of transactions
-    for row in source.read().splitlines():
-        record = ingest_record(row)
-        if not record:
-            continue
-        # New donor
-        if record['donor_id'] not in donor_dict:
-            donor_dict[record['donor_id']] = record['transaction_yr']
-        # update transaction_yr if you come across donation from a previous year
-        elif record['transaction_yr'] < donor_dict[record['donor_id']]:
-            donor_dict[record['donor_id']] = record['transaction_yr']
-        elif record['transaction_yr'] > donor_dict[record['donor_id']]:
-            # Accumulate contributions as a list in the dictionary
-            donation_dict[record['recipient_id']].append(record['transaction_amt'])
-            transaction_cnt = len(donation_dict[record['recipient_id']])
-            contribution = sum(donation_dict[record['recipient_id']])
-            pct = compute_percentile(donation_dict[record['recipient_id']], percentile)
-            output = [record['recipient'], record['zip_code'], str(record['transaction_yr']), str(pct), str(contribution), str(transaction_cnt)]
-            write_to_destination(output, destination, '|')
+def ingest_record(row):
+    cols = row.split("|")
+    record = {}
+    # Create a dictionary with fields for easy access
+    record['recipient'] = cols[0]
+    record['donor'] = cols[7]
+    record['zip_code'] = cols[10][:5]
+    record['transaction_dt'] = cols[13]
+    record['transaction_yr'] = cols[13][4:]
+    record['transaction_amt'] = cols[14]
+    record['other_id'] = cols[15]
+    record['donor_id'] = record['donor'] + record['zip_code']
+    record['recipient_id'] = record['recipient'] + record['zip_code'] + str(record['transaction_yr'])
+    if valid_record(record):
+        process_record(record)
+
+def process_record(record):
+    transaction_yr = int(record['transaction_yr'])
+    transaction_amt = int(round(float(record['transaction_amt'])))
+    # New Donor
+    if record['donor_id'] not in donor_dict:
+        donor_dict[record['donor_id']] = transaction_yr
+    # update transaction_yr if you come across donation from a previous year
+    elif transaction_yr < donor_dict[record['donor_id']]:
+        donor_dict[record['donor_id']] = transaction_yr
+    # Repeat Donor
+    elif transaction_yr > donor_dict[record['donor_id']]:
+        donation_dict[record['recipient_id']].append(transaction_amt)
+        compute_stats(record)
+
+def compute_stats(record):
+    trans_cnt = len(donation_dict[record['recipient_id']])
+    total_donation = sum(donation_dict[record['recipient_id']])
+    pct = compute_percentile(donation_dict[record['recipient_id']], percentile)
+    output = [record['recipient'], record['zip_code'], str(record['transaction_yr']), str(pct), str(total_donation), str(trans_cnt)]
+    write_to_destination(output, destination, '|')
 
 if __name__ == "__main__":
     try:
@@ -74,5 +71,10 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
         sys.exit(1)
-    process_data(source, destination)
+    # Data Structures
+    donor_dict = {} # Key: donor_id | Value: transaction_yr
+    donation_dict = defaultdict(list) # Key: recipient | Value: list of transactions
+    for row in source.read().splitlines():
+        ingest_record(row)
+
     clean_up()
